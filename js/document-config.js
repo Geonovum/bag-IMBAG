270ad4a44a41b1f27b27f6cc8776d5c62b2635bf
Gerard Wolbers <g.wolbers@geonovum.nl>
2025-07-01 14:05:19 +0200

    Initial commit

---
+++ js/document-config.js
@@ -0,0 +1,40 @@
+var documentConfig =
+{
+  title: "Catalogus Basisregristratie Adressen en Gebouwen",
+  subtitle: "catalogus IMBAG",
+  shortName: "IMBAG",
+  pubDomain: "bag",
+  specStatus: "wv",
+  specType: "st",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/bag/IMBAG/"
+  ],
+  publishDate: "2025-07-01",
+  publishVersion: "2.1.0",
+  thisVersion: [
+    "https://geonovum.github.io/bag-IMBAG/"
+  ],
+  github: "https://github.com/geonovum/bag-IMBAG/",
+  issueBase: "https://github.com/geonovum/bag-IMBAG/issues/",
+  edDraftURI: "https://geonovum.github.io/bag-IMBAG/",
+  canonicalURI: "https://docs.geostandaarden.nl/bag/IMBAG/",
+  editors: [
+    {
+      name: "Geonovum",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  localBiblio: {
+    "BAG": {
+      title: "IMBAG",
+      href: "",
+      authors: "",
+      publisher: "",
+      editors: "",
+      date: "",
+      status: ""
+    }
+  }
+}
